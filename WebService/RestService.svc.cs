879dbcb4223c679c27780fb84880a1a0c828335f
Gabriel Batista de Lima <gabrielrambo_@hotmail.com>
2013-06-24 21:53:21 -0300

    Update RestService.svc.cs

--- WebService/RestService.svc.cs
+++ WebService/RestService.svc.cs
@@ -1,4 +1,4 @@
-﻿using BusinessObject.Voting;
+using BusinessObject.Voting;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -18,82 +18,102 @@ namespace WebService
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
     public class RestService : IRestService
     {
-
-        public void Seeds()
+        
+        private string GetError(string message)
         {
-            new Seeds();
+            return "Erro. Detalhes: " + message;
         }
 
-        /*
-        public string Registration()
+        /// <summary>
+        /// http://localhost:62425/RestService.svc/Seeds
+        /// </summary>
+        public string Seeds()
         {
-            Registration registration = null;
-            //Guid token = Guid.Empty;
             try
             {
-                registration = new Registration();
-                new RegistrationController().Save(registration);
-                //token = Guid.NewGuid();
-                //new RegistrationController().Save(token);
+                new Seeds();
+                return "Seeds OK";
             }
             catch (Exception e)
             {
-                
+                return this.GetError(e.Message);
             }
-            return JsonConvert.SerializeObject(registration);
-        }*/
+        }
 
+        /// <summary>
+        /// http://localhost:62425/RestService.svc/AllVotes
+        /// </summary>
         public string AllVotes()
         {
-            return JsonConvert.SerializeObject(
-                new VoteController().SelectAll<Vote>());
-        }
-
-        public string CurrentApresentation(string date)
-        {
-            return JsonConvert.SerializeObject(
-                new ApresentationController().Select(DateTime.Now));
-        }
-
-        public string UploadVote(string vote)
-        {
-            JsonResponse response = new JsonResponse();
-            var result = false;
             try
             {
-                var voteConvert = JsonConvert.DeserializeObject<Vote>(vote);
-                new VoteController().Save(voteConvert);
-                response.Status = true;
-                result = true;
+                return JsonConvert.SerializeObject(
+                    new VoteController().SelectAll<Vote>());
             }
-            catch (Exception) { }
-
-            return JsonConvert.SerializeObject(result);
+            catch (Exception e)
+            {
+                return this.GetError(e.Message);
+            }
         }
 
-        public string AllPresentations()
+        /// <summary>
+        /// http://localhost:62425/RestService.svc/CurrentApresentation?date=24-06-2013%2021:32:00
+        /// </summary>
+        public string CurrentApresentation(string interval)
         {
-            var d = JsonConvert.SerializeObject(
-                new ApresentationController().SelectAll<Apresentation>(),
-                new JsonSerializerSettings { StringEscapeHandling = StringEscapeHandling.Default, FloatFormatHandling = FloatFormatHandling.String });
-
-            var e = JsonConvert.SerializeObject(
-                new ApresentationController().SelectAll<Apresentation>(),
-                new JsonSerializerSettings { StringEscapeHandling = StringEscapeHandling.EscapeHtml });
-
-            var a = JsonConvert.SerializeObject(
-                new ApresentationController().SelectAll<Apresentation>(),
-                new JsonSerializerSettings { StringEscapeHandling = StringEscapeHandling.EscapeNonAscii });
-
-            return JsonConvert.SerializeObject(
-                new ApresentationController().SelectAll<Apresentation>());
+            try
+            {
+                return JsonConvert.SerializeObject(
+                    new ApresentationController().Current(
+                        Convert.ToDateTime(interval)));
+            }
+            catch (Exception e)
+            {
+                return this.GetError(e.Message);
+            }
         }
 
-
-
-        public string GetTest()
+        /// <summary>
+        /// 
+        /// </summary>
+        public string UploadVote(string vote)
         {
-            return "OK";
+            try
+            {
+                new VoteController().Save(
+                    JsonConvert.DeserializeObject<Vote>(vote));
+                return "Voto computado com sucesso!";
+            }
+            catch (Exception e)
+            {
+                return this.GetError(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// http://localhost:62425/RestService.svc/HistoryPresentations?date=24-06-2013%2021:22:00
+        /// </summary>
+        public string HistoryPresentations(string interval)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(
+                    new ApresentationController().History(
+                        Convert.ToDateTime(interval)));
+            }
+            catch (Exception e)
+            {
+                return this.GetError(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// http://localhost:62425/RestService.svc/GetConnection
+        /// </summary>
+        public string GetConnection()
+        {
+            return "true";
         }
     }
 }
+
